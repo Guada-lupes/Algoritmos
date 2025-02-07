82ae7adba3f3a9132f2b363b05b54cbb3b45d7cb
Guada-lupes <gma.pedagoga@gmail.com>
2025-02-07 21:06:26 +0000

    ejercicio 15 terminado y mitad del ejercicio 16

--- src/ALGORITMOS/15.js
+++ src/ALGORITMOS/15.js
@@ -1,1 +1,24 @@
 //Crear un programa que devuelva los números primos hasta el número (n).
+function numerosPrimos(numero){
+
+    //Creamos un array para guardar los restos
+        let arrayRestos = [];
+
+    for(let i = 1; i<=numero; i++){
+        //Dividimos el numero por todos los números más pequeños hasta el 1
+        const resto = numero% i;
+//Si el resto es igual a 0 lo añadimos al array
+        if(resto === 0){
+            arrayRestos.push(resto);   
+        }
+    }
+//Si el array es mayor de 2, significa que el numero es divisible por más de dos numeros, lo que lo hace no primo
+    if(arrayRestos.length === 2){
+        console.log(`${numero } es un número primo`);
+    }
+    else{
+        console.log(`${numero } NO es un número primo`);
+        
+    }
+}
+numerosPrimos(25);
--- src/ALGORITMOS/16.js
+++ src/ALGORITMOS/16.js
@@ -1,1 +1,48 @@
 //Escribir una función que convierta un número romano a número arábigo.
+
+function arabicoRomano(numero) {
+  const equivalencias = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+  console.log(equivalencias);
+
+  const numeroVerificacion = parseInt(numero);
+  console.log(typeof numeroVerificacion);
+
+  if (!isNaN(numeroVerificacion)) {
+    //conversion a romano
+    console.log("es numero");
+  }
+  if (isNaN(numeroVerificacion)) {
+    //conversion a arabico
+    const numeroToArray = numero.toUpperCase().split("");
+    let valoresConvertidos = [];
+    let valoresAsignados = []
+    console.log("es string");
+    numeroToArray.forEach((elemento) => {
+
+      if (equivalencias.hasOwnProperty(elemento)) {
+        console.log(equivalencias[elemento]);
+        valoresConvertidos.push(equivalencias[elemento])
+      }})
+
+      for (let i = 0; i <valoresConvertidos.length; i++){
+        console.log(valoresConvertidos);
+        console.log(i);
+        
+        if(valoresConvertidos[i]<valoresConvertidos[i+1]){
+            console.log(-valoresConvertidos[i]);
+            
+            valoresAsignados.push(-valoresConvertidos[i])
+        }else{
+            valoresAsignados.push(valoresConvertidos[i])
+            console.log(valoresConvertidos[i]);
+        }
+        console.log(valoresAsignados);
+      }
+    const resultado = valoresAsignados.reduce((acumulador, valorActual)=> acumulador+valorActual, 0)
+    
+    console.log(resultado);
+    
+  }
+}
+
+arabicoRomano("ivx");
